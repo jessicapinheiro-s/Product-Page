04659e90958ca6836fcc144dfddefe5c06fb0ed5
Jessica Pinheiro <jessicasilva.js1314@gmail.com>
2023-03-04 12:22:45 -0300

    criando menu responsivo

---
+++ Produto page/js/app.js
@@ -0,0 +1,4 @@
+document.getElementById('icon-menu-responsivo').addEventListener('click', function openMenu(){
+    document.querySelector('.menu-conteiner-responsive').classList.toggle('mostrar');
+})
+
